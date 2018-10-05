510be09111200e8ec536ab9ac3b272f0dbf2da63
Lychwee <weejiaquan1234@gmail.com>
2018-10-05 12:51:31 -0700

    Updated Auto Login Script
    Updated Auto Login script to adapt to the newer UI.

---
+++ Auto Login (Columbia College) Student.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Auto Login (Columbia College) Student
+// @namespace    Lychwee
+// @version      1
+// @description  try to take over the world!
+// @author       Lychwee
+// @match        *captiveportal-login.columbiacollege.bc.ca/swarm.cgi?*
+// EDIT @match to match with your portal login URL
+// @grant        none
+// ==/UserScript==
+
+
+
+//Change USERID and PASSWORD accordingly
+window.onload = function(){
+    check();
+}
+function check() {
+    document.getElementById("agree-yes").checked = true;
+    document.getElementById("user").value = "804524";
+    document.getElementById("password").value = "cc!030799";
+    document.getElementsByTagName("input")[5].click();
+}
